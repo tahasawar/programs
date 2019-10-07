d0bcc6e950710b633ad7ac5cfe0fece9ac7afcc2
taha <taha@192.168.0.196>
2019-10-07 03:38:48 -0500

    First Commit
    Uploading all files

---
+++ P02 Matching Game/src/MatchingGame.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.util.Random;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title:           MatchingGame
+// Files:           MatchingGame.java
+// Course:          300, Fall, 2019
+//
+// Author:          Taha Sawar
+// Email:           tsawar@wisc.edu
+// Lecturer's Name: Gary Dahl
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name:    Matthew Robert Kottwitz
+// Partner Email:   mrkottwitz@wisc.edu
+// Partner Lecturer's Name: Mouna Kacem
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   X Write-up states that pair programming is allowed for this assignment.
+//   X We have both read and understand the course Pair Programming Policy.
+//   X We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully 
+// acknowledge and credit those sources of help here.  Instructors and TAs do 
+// not need to be credited here, but tutors, friends, relatives, room mates, 
+// strangers, and others do.  If you received no outside help from either type
+//  of source, then please explicitly indicate NONE.
+//
+// Persons:          NONE
+// Online Sources:   NONE
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+
+/**
+ * @author taha
+ *
+ */
+
+
+public class MatchingGame {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    
+    Utility.runApplication(); // starts the application
+
+  }
+
+}
